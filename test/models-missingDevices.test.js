37bb674ed415c27e07fcfe0ec83961fe9938c435
Sascha Hölzel <mrb1232@posteo.de>
2026-06-02 21:44:43 +0200

    refactor: clean up model configuration

--- test/models-missingDevices.test.js
+++ test/models-missingDevices.test.js
@@ -40,17 +40,6 @@ function createMockVacbot(deviceClass, modelType, overrides = {}) {
 }
 
 describe('Missing device class support in SUPPORTED_STATES', () => {
-    describe('p95mgv (DEEBOT T10 PLUS)', () => {
-        const deviceClass = 'p95mgv';
-        const modelType = 'T10';
-
-        it('should resolve product name via deviceClassLink', () => {
-            const vacbot = createMockVacbot(deviceClass, modelType);
-            const model = new Model(vacbot, {});
-            expect(model.getProductName()).to.equal('DEEBOT T10 PLUS');
-        });
-    });
-
     describe('20anby (Z1 Air Quality Monitor)', () => {
         const deviceClass = '20anby';
         const modelType = 'aqMonitor';
@@ -61,31 +50,6 @@ describe('Missing device class support in SUPPORTED_STATES', () => {
             expect(model.getProductName()).to.equal('Z1 Air Quality Monitor');
         });
     });
-
-    describe('9eamof (DEEBOT T80 OMNI)', () => {
-        const deviceClass = '9eamof';
-        const modelType = 'T10';
-
-        it('should resolve product name via deviceClassLink', () => {
-            const vacbot = createMockVacbot(deviceClass, modelType);
-            const model = new Model(vacbot, {});
-            expect(model.getProductName()).to.equal('DEEBOT T80 OMNI');
-        });
-    });
-
-    describe('p1jij8 (DEEBOT T20 OMNI)', () => {
-        const deviceClass = 'p1jij8';
-        const modelType = 'T20';
-
-        it('should resolve product name via deviceClassLink', () => {
-            const vacbot = createMockVacbot(deviceClass, modelType, {
-                isModelTypeT20: sinon.stub().returns(true)
-            });
-            const model = new Model(vacbot, {});
-            expect(model.getProductName()).to.equal('DEEBOT T20 OMNI');
-        });
-    });
-
 
     describe('sdp1y1 (AIRBOT Z1)', () => {
         const deviceClass = 'sdp1y1';
@@ -99,21 +63,6 @@ describe('Missing device class support in SUPPORTED_STATES', () => {
             });
             const model = new Model(vacbot, {});
             expect(model.getProductName()).to.equal('AIRBOT Z1');
-        });
-    });
-
-    describe('xmp9ds (GOAT A1600 RTK)', () => {
-        const deviceClass = 'xmp9ds';
-        const modelType = 'goat';
-
-        it('should resolve product name via deviceClassLink', () => {
-            const vacbot = createMockVacbot(deviceClass, modelType, {
-                hasMappingCapabilities: sinon.stub().returns(false),
-                hasMoppingSystem: sinon.stub().returns(false),
-                is950type: sinon.stub().returns(false)
-            });
-            const model = new Model(vacbot, {});
-            expect(model.getProductName()).to.equal('GOAT A1600 RTK');
         });
     });
 });
